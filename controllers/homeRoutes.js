cfea91b40e5e2c9f4a00d52e0da74f086d4bfe68
Colin Brooks <mcbrooks@pm.me>
2022-06-14 00:40:38 -0400

    added login and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,72 +1,26 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    // const projectData = await Project.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
     // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    const blogs = [
-      {
-        id: 1,
-        title: "title",
-        content: "content",
-        author: "author",
-        date: "1/17/2022"
-      },
-      {
-        id: 1,
-        title: "title2",
-        content: "content2",
-        author: "author",
-        date: "1/17/2022"
-      },
-      {
-        id: 1,
-        title: "title3",
-        content: "content3",
-        author: "author",
-        date: "1/17/2022"
-      }
-    ]
+    const blogs = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       blogs, 
-      logged_in: true, 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    // const projectData = await Project.findByPk(req.params.id, {
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
-
-    // const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in, 
     });
   } catch (err) {
     res.status(500).json(err);
@@ -74,44 +28,43 @@ router.get('/project/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-// router.get('/dashboard', withAuth, async (req, res) => {
-  router.get('/dashboard', async (req, res) => {
+  router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    // const userData = await User.findByPk(req.session.user_id, {
-    //   attributes: { exclude: ['password'] },
-    //   include: [{ model: Project }],
-    // });
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
 
-    // const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-    const blogs = [
-      {
-        id: 1,
-        title: "title",
-        content: "content",
-        author: "author",
-        date: "1/17/2022"
-      },
-      {
-        id: 1,
-        title: "title2",
-        content: "content2",
-        author: "author",
-        date: "1/17/2022"
-      },
-      {
-        id: 1,
-        title: "title3",
-        content: "content3",
-        author: "author",
-        date: "1/17/2022"
-      }
-    ]
+    // const blogs = [
+    //   {
+    //     id: 1,
+    //     title: "title",
+    //     content: "content",
+    //     author: "author",
+    //     date: "1/17/2022"
+    //   },
+    //   {
+    //     id: 1,
+    //     title: "title2",
+    //     content: "content2",
+    //     author: "author",
+    //     date: "1/17/2022"
+    //   },
+    //   {
+    //     id: 1,
+    //     title: "title3",
+    //     content: "content3",
+    //     author: "author",
+    //     date: "1/17/2022"
+    //   }
+    // ]
 
     res.render('dashboard', {
-      blogs,
-      logged_in: true
+      ...user,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -136,7 +89,7 @@ router.get('/create-blog', (req, res) => {
   // }
 
   res.render('create-blog', {
-    logged_in: true
+    logged_in: req.session.logged_in
   });
 });
 
@@ -157,40 +110,52 @@ router.get('/edit-blog', (req, res) => {
 
   res.render('edit-blog', {
     ...blog,
-    logged_in: true
+    logged_in: req.session.logged_in
   });
 });
 
-router.get('/blogs/:id', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect('/profile');
-  //   return;
-  // }
+router.get('/blogs/:id', async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+      {
+        model: Comment,
+        include: {
+          model: User,
+          attributes: ['username'],
+        }
+      }
+    ],
+  });
 
-  const blog = {
-    id: "id",
-    title: "title3",
-    content: "content3",
-    author: "author",
-    date: "1/17/2022",
-    comments: [
-      {
-        content: "this is a comment",
-        author: "author",
-        date: "1/17/2022"
-      },
-      {
-        content: "this is a comment2",
-        author: "author2",
-        date: "1/18/2022"
-      },
-    ]
-  }
+  const blog = postData.get({ plain: true });
+
+  // const blog = {
+  //   id: "id",
+  //   title: "title3",
+  //   content: "content3",
+  //   author: "author",
+  //   date: "1/17/2022",
+  //   comments: [
+  //     {
+  //       content: "this is a comment",
+  //       author: "author",
+  //       date: "1/17/2022"
+  //     },
+  //     {
+  //       content: "this is a comment2",
+  //       author: "author2",
+  //       date: "1/18/2022"
+  //     },
+  //   ]
+  // }
 
   res.render('blog-page', {
     ...blog,
-    logged_in: true
+    logged_in: req.session.logged_in
   });
 });
 
