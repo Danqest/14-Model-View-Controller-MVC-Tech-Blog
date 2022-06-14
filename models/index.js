fefc9249bb3e49b96b0b087003313f39b2469c5f
Colin Brooks <mcbrooks@pm.me>
2022-06-13 23:15:05 -0400

    added new models

--- models/index.js
+++ models/index.js
@@ -7,6 +7,11 @@ Post.belongsTo(User, {
   onDelete: 'CASCADE',
 });
 
+User.hasMany(Post, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+})
+
 Post.hasMany(Comment, {
   foreignKey: 'postId',
   onDelete: 'CASCADE',
@@ -16,6 +21,11 @@ Comment.belongsTo(User, {
   foreignKey: 'userId',
   onDelete: 'CASCADE',
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+})
 
 module.exports = { 
   User, 
